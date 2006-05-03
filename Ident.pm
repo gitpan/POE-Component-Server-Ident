056eedd944034f0ffafdc23a5e2fa6bd8abe268c
Chris Williams <chris@bingosnet.co.uk>
2006-05-03 11:11:57 +0000

    Import of BINGOS/POE-Component-Server-Ident-1.00 from CPAN.
    gitpan-cpan-distribution: POE-Component-Server-Ident
    gitpan-cpan-version:      1.00
    gitpan-cpan-path:         BINGOS/POE-Component-Server-Ident-1.00.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

--- Ident.pm
+++ Ident.pm
@@ -1,5 +1,6 @@
 package POE::Component::Server::Ident;
 
+use 5.006;
 use strict;
 use warnings;
 use POE qw( Wheel::SocketFactory Wheel::ReadWrite Driver::SysRW
@@ -8,7 +9,7 @@ use Carp;
 use Socket;
 use vars qw($VERSION);
 
-$VERSION = '0.3';
+$VERSION = '1.00';
 
 use constant PCSI_REFCOUNT_TAG => "P::C::S::I registered";
 
@@ -24,7 +25,7 @@ sub spawn {
 
   my $self = $package->new($alias,$bindaddr,$bindport,$multiple,$timeout);
 
-  POE::Session->create (
+  $self->{session_id} = POE::Session->create (
 	object_states => [
 		$self => { _start     => 'server_start',
 			   _stop      => 'server_stop',
@@ -32,7 +33,7 @@ sub spawn {
 			 },
 		$self => [ qw(add_connection del_connection accept_new_client accept_failed register unregister) ],
 	],
-  );
+  )->ID();
   return $self;
 }
 
@@ -57,9 +58,10 @@ sub getsockname {
 }
 
 sub server_start {
-  my ($kernel,$self) = @_[KERNEL,OBJECT];
+  my ($kernel,$self,$session) = @_[KERNEL,OBJECT,SESSION];
 
   $kernel->alias_set( $self->{Alias} );
+  $self->{session_id} = $session->ID();
 
   $self->{listener} = POE::Wheel::SocketFactory->new ( 
 			BindPort => $self->{BindPort},
@@ -79,20 +81,16 @@ sub server_stop {
 
 sub server_close {
   my ($kernel,$self) = @_[KERNEL,OBJECT];
-
-  foreach ( keys %{ $self->{clients} } ) {
-	delete ( $self->{clients}->{ $_ }->{readwrite} );
-  }
-  delete ( $self->{listener} );
   $kernel->alias_remove( $self->{Alias} );
+  delete $self->{clients}->{ $_ }->{readwrite} for keys %{ $self->{clients} };
+  delete $self->{listener};
+  $self->_unregister($self->{session_id},$_) for keys %{ $self->{sessions} };
   undef;
 }
 
 sub accept_new_client {
   my ($kernel,$self,$socket,$peeraddr,$peerport) = @_[KERNEL,OBJECT,ARG0 .. ARG2];
   $peeraddr = inet_ntoa($peeraddr);
-
-  # $self->{clients}->{ $peeraddr }->{ $peerport }->{Socket} = $socket;
 
   POE::Session->create (
 	object_states => [
@@ -108,7 +106,7 @@ sub accept_new_client {
 
 sub accept_failed {
   my ($kernel,$self,$function,$error) = @_[KERNEL,OBJECT,ARG0,ARG2];
-  my ($package) = ref $self;
+  my $package = ref $self;
 
   $kernel->call ( $self->{Alias} => 'shutdown' );
 
@@ -131,13 +129,17 @@ sub register {
 
 sub unregister {
   my ($kernel,$self,$sender,$session) = @_[KERNEL,OBJECT,SENDER,SESSION];
-  $sender = $sender->ID();
-  $session = $session->ID();
+  $self->_unregister($session->ID(),$sender->ID());
+  undef;
+}
+
+sub _unregister {
+  my ($self,$session,$sender) = splice @_,0,3;
 
   if (--$self->{sessions}->{$sender}->{refcnt} <= 0) {
       delete $self->{sessions}->{$sender};
       unless ($session == $sender) {
-        $kernel->refcount_decrement($sender, PCSI_REFCOUNT_TAG);
+        $poe_kernel->refcount_decrement($sender, PCSI_REFCOUNT_TAG);
       }
   }
   undef;
@@ -145,18 +147,19 @@ sub unregister {
 
 sub client_start {
   my ($kernel,$session,$self,$socket,$peeraddr,$peerport) = @_[KERNEL,SESSION,OBJECT,ARG0,ARG1,ARG2];
+  my $session_id = $session->ID();
   
-  $self->{clients}->{ $session->ID }->{Socket} = $socket;
-  $self->{clients}->{ $session->ID }->{PeerAddr} = $peeraddr;
-  $self->{clients}->{ $session->ID }->{PeerPort} = $peerport;
+  $self->{clients}->{ $session_id }->{Socket} = $socket;
+  $self->{clients}->{ $session_id }->{PeerAddr} = $peeraddr;
+  $self->{clients}->{ $session_id }->{PeerPort} = $peerport;
 
-  $self->{clients}->{ $session->ID }->{readwrite} = 
+  $self->{clients}->{ $session_id }->{readwrite} = 
   POE::Wheel::ReadWrite->new(
 	Handle => $socket,
 	Filter => POE::Filter::Line->new( Literal => "\x0D\x0A" ),
 	InputEvent => 'client_input',
 	ErrorEvent => 'client_error',
-	( $self->{'Multiple'} == 1 ? () : ( FlushedEvent => 'client_timeout' ) ),
+	( $self->{'Multiple'} ? () : ( FlushedEvent => 'client_timeout' ) ),
   );
 
   # Set a delay to close the connection if we are idle for 60 seconds.
@@ -175,19 +178,20 @@ sub client_stop {
 
 sub client_input {
   my ($kernel,$self,$session,$input) = @_[KERNEL,OBJECT,SESSION,ARG0];
+  my $session_id = $session->ID();
 
   # Parse what is passed. We want <number>,<number> or nothing.
 
   if ( $input =~ /^\s*([0-9]+)\s*,\s*([0-9]+)\s*$/ and _valid_ports($1,$2) ) {
-    my ($port1) = $1; my ($port2) = $2;
-    $self->{clients}->{ $session->ID }->{'Port1'} = $port1;
-    $self->{clients}->{ $session->ID }->{'Port2'} = $port2;
+    my $port1 = $1; my $port2 = $2;
+    $self->{clients}->{ $session_id }->{'Port1'} = $port1;
+    $self->{clients}->{ $session_id }->{'Port2'} = $port2;
     # Okay got a sort of valid query. Send it to all interested sessions.
-    $kernel->call( $_ => 'identd_request' => $self->{clients}->{ $session->ID }->{PeerAddr} => $port1 => $port2 ) for keys %{ $self->{sessions} };
+    $kernel->call( $_ => 'identd_request' => $self->{clients}->{ $session_id }->{PeerAddr} => $port1 => $port2 ) for keys %{ $self->{sessions} };
     $kernel->delay ( 'client_default' => 10 );
   } else {
     # Client sent us rubbish.
-    $self->{clients}->{ $session->ID }->{readwrite}->put("0 , 0 : ERROR : INVALID-PORT");
+    $self->{clients}->{ $session_id }->{readwrite}->put("0 , 0 : ERROR : INVALID-PORT");
   }
   $kernel->delay ( 'client_timeout' => $self->{'TimeOut'} ) if ( $self->{'Multiple'} );
   undef;
@@ -227,22 +231,24 @@ sub del_connection {
 
 sub client_default {
   my ($kernel,$self,$session) = @_[KERNEL,OBJECT,SESSION];
+  my $session_id = $session->ID();
 
-  my ($reply) = $self->{clients}->{ $session->ID }->{'Port1'} . " , " . $self->{clients}->{ $session->ID }->{'Port2'} . " : ERROR : HIDDEN-USER";
+  my $reply = $self->{clients}->{ $session_id }->{'Port1'} . " , " . $self->{clients}->{ $session_id }->{'Port2'} . " : ERROR : HIDDEN-USER";
 
-  $self->{clients}->{ $session->ID }->{readwrite}->put($reply) if ( defined ( $self->{clients}->{ $session->ID }->{readwrite} ) );
+  $self->{clients}->{ $session_id }->{readwrite}->put($reply) if defined ( $self->{clients}->{ $session_id }->{readwrite} );
   $kernel->delay ( 'client_timeout' => $self->{'TimeOut'} ) if ( $self->{'Multiple'} );
   undef;
 }
 
 sub ident_server_reply {
   my ($kernel,$self,$session) = @_[KERNEL,OBJECT,SESSION];
+  my $session_id = $session->ID();
 
   my ($opsys,$userid) = @_[ARG0 .. ARG1];
 
   $opsys = "UNIX" unless defined ( $opsys );
 
-  my ($reply) = $self->{clients}->{ $session->ID }->{'Port1'} . " , " . $self->{clients}->{ $session->ID }->{'Port2'} . " : USERID : " . $opsys . " : " . $userid;
+  my $reply = $self->{clients}->{ $session->ID }->{'Port1'} . " , " . $self->{clients}->{ $session->ID }->{'Port2'} . " : USERID : " . $opsys . " : " . $userid;
 
   $self->{clients}->{ $session->ID }->{readwrite}->put($reply);
   $kernel->delay ( 'client_timeout' => $self->{'TimeOut'} ) if ( $self->{'Multiple'} );
@@ -252,16 +258,17 @@ sub ident_server_reply {
 
 sub ident_server_error {
   my ($kernel,$self,$session,$error_type) = @_[KERNEL,OBJECT,SESSION,ARG0];
+  my $session_id = $session->ID();
   $error_type = uc $error_type;
 
   unless ( grep {$_ eq $error_type} qw(INVALID-PORT NO-USER HIDDEN-USER UNKNOWN-ERROR) ) {
 	$error_type = 'UNKNOWN-ERROR';
   }
 
-  my ($reply) = $self->{clients}->{ $session->ID }->{'Port1'} . " , " . $self->{clients}->{ $session->ID }->{'Port2'} . " : ERROR : " . $error_type;
+  my $reply = $self->{clients}->{ $session_id }->{'Port1'} . " , " . $self->{clients}->{ $session_id }->{'Port2'} . " : ERROR : " . $error_type;
 
-  $self->{clients}->{ $session->ID }->{readwrite}->put($reply);
-  $kernel->delay ( 'client_timeout' => $self->{'TimeOut'} ) if ( $self->{'Multiple'} );
+  $self->{clients}->{ $session_id }->{readwrite}->put($reply);
+  $kernel->delay ( 'client_timeout' => $self->{'TimeOut'} ) if $self->{'Multiple'};
   $kernel->delay ( 'client_default' => undef );
   undef;
 }
@@ -295,24 +302,34 @@ POE::Component::Server::Ident - A component that provides non-blocking ident ser
 
 =head1 SYNOPSIS
 
-   use POE::Component::Server::Ident;
+   use strict;
+   use warnings;
+   use POE qw(Component::Server::Ident);
 
    POE::Component::Server::Ident->spawn ( Alias => 'Ident-Server' );
 
    POE::Session->create ( 
-		inline_states => {
-					identd_request => \&ident_request_handler,
-				 },
+	package_states => [
+		'main' => [qw(_start identd_request)],
+	],
    );
 
-   $kernel->post ( 'Ident-Server' => 'register' );
+   $poe_kernel->run();
+   exit 0;
 
-   $kernel->post ( 'Ident-Server' => 'unregister' );
+   sub _start {
+      $poe_kernel->post( 'Ident-Server' => 'register' );
+      undef;
+   }
 
-   sub ident_request_handler {
+
+   sub identd_request {
       my ($kernel,$sender,$peeraddr,$port1,$port2) = @_[KERNEL,SENDER,ARG0,ARG1,ARG2];
-
-      $kernel->call ( $sender => ident_server_reply => 'UNIX' => 'lameuser' );
+      my ($val1,$val2);
+      $val1 = $val2 = int(rand(99999));
+      $val1 =~ tr/0-9/A-Z/;
+      $kernel->call ( $sender => ident_server_reply => 'OTHER' => "$val1$val2" );
+      undef;
    }
 
 =head1 DESCRIPTION
@@ -329,18 +346,23 @@ sent.
 The component will automatically respond to the client requests with 'ERROR : HIDDEN-USER' if your 
 sessions do not send a reponse within a 10 second timeout period.
 
-=head1 METHODS
+=head1 CONSTRUCTOR
 
 =over
 
 =item spawn
 
-Takes a number of arguments: 'Alias', a kernel alias to address the component with; 'BindAddr',
-the IP address that the component should bind to, defaults to INADDR_ANY; 'BindPort', the port 
-that the component will bind to, default is 113; 'Multiple', specify whether the component should
-allow multiple ident queries from clients by setting this to 1, default is 0 which terminates client 
-connections after a response has been sent; 'TimeOut', this is the idle timeout on client connections, 
-default is 60 seconds, excepts values between 60 and 180 seconds.
+Takes a number of arguments: 
+
+  'Alias',    a kernel alias to address the component with; 
+  'BindAddr', the IP address that the component should bind to, 
+	      defaults to INADDR_ANY;
+  'BindPort', the port that the component will bind to, default is 113;
+  'Multiple', specify whether the component should allow multiple ident queries 
+              from clients by setting this to 1, default is 0 which terminates 
+              client connections after a response has been sent;
+  'TimeOut',  this is the idle timeout on client connections, default
+              is 60 seconds, accepts values between 60 and 180 seconds.
 
 =back
 
@@ -366,6 +388,10 @@ Takes two arguments, the first is the 'opsys' field of the ident response, the s
 =item ident_server_error
 
 Takes one argument, the error to return to the client.
+
+=item shutdown
+
+Terminates the component. The listener is closed and all current client connections are disconnected.
 
 =back
 
